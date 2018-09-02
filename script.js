c3960affc9653a84f0802c9e01d8a57a49fd6f0a
Ismail <mailismailpe@gmail.com>
2018-09-02 17:47:20 +0530

    made use of list of objects

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var toDoAddButton = document.getElementById("addTodoItem");
 var toDoElement = document.getElementById("todoList");
 var todoTextBox = document.getElementById("newTodoItemText");
+var todos = [];
 function addTodo(){
 	var todoText=todoTextBox.value.toString();
 	if(todoText.length > 2){
 		todoTextBox.classList.toggle("NoMinText",false);
-		randomId=Math.random();
+		randomId=Math.random()*100;
+		todos.push({id:randomId,item:todoText,status:"ToDoToDo"});
+		console.log(todos.length);
 		toDoElement.innerHTML += "<li class='ToDoToDo' id='" + randomId + "' onClick='doneTodo(this)'>" + todoText + "<input class='ToDoToDo' type='button' id='" + randomId + "' value='x' onClick='deleteTodo(this)'></li>";
 		document.getElementById(randomId).classList.add('ToDoToDo');
 		todoTextBox.value="";
@@ -17,6 +20,12 @@ function addTodo(){
 }
 function deleteTodo(toDelete){
 	toDoElement.removeChild(toDelete.parentElement);
+	todoDelete=todos.filter(function (t){
+		return t.id==toDelete.id;
+	});
+	//console.log(todoDelete);
+	todos.splice(todos.indexOf(todoDelete[0]),1);
+//	todos.splice(todos.indexOf(todoDelete[0].item),1);
 }
 function doneTodo(toToggle){
 	console.log(toToggle.classList);
